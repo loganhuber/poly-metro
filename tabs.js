5f8c88db4b3f71e884812d16518e95d31942fd0c
Logan <cjhuber102@mail.com>
2026-04-07 11:53:20 -0500

    tabs fade in and out

--- tabs.js
+++ tabs.js
@@ -4,10 +4,20 @@ const tabs = document.querySelectorAll('.tab');
 tabBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         const target = btn.getAttribute('data-target');
+
+        // underline selected tab button
+        tabBtns.forEach(b => b.classList.remove('selected'));
+        btn.classList.add('selected');
+
+        // switch tabs
         tabs.forEach(tab => {
             if (tab.id === target) {
                 tab.classList.remove('hidden');
-                tab.classList.add('visible');
+                tab.classList.add('invisible');
+                setTimeout(() => {
+                    tab.classList.remove('invisible');
+                    tab.classList.add('visible');
+                }, 50)
             } else {
                 tab.classList.remove('visible');
                 tab.classList.add('hidden');
